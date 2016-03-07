61475f9cabb53eaab7a88aa3e1a3647cc4a1fae9
Andrew Farrar <kegwen@gmail.com>
2016-03-07 14:29:14 -0500

    add/remove streams

--- client/streams/streams-list/streams-list.component.js
+++ client/streams/streams-list/streams-list.component.js
@@ -6,13 +6,25 @@ angular.module('relayer').directive('streamsList', function() {
         controller: function($scope, $reactive) {
             $reactive(this).attach($scope);
 
+            this.newStream = {};
+
             this.subscribe('streams');
 
             this.helpers({
                 streams: () => {
                     return Streams.find({});
                 }
-            })
+            });
+
+            this.addStream = () => {
+                this.newStream.creator = Meteor.user()._id;
+                Streams.insert(this.newStream);
+                this.newStream = {};
+            };
+
+            this.removeStream = (stream) => {
+                Streams.remove({ _id: stream._id });
+            };
         }
     }
 })
