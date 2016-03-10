117e7f9fd782e3e10ef66ca661a8cf5190ca6f0c
Andrew Farrar <kegwen@gmail.com>
2016-03-10 11:43:43 -0500

    janky first user solution

---
+++ server/startup/loadUser.js
@@ -0,0 +1,18 @@
+Meteor.startup(function () {
+    if (Meteor.users.find().count() === 0) {
+        let firstUser = {
+            email: 'test@test.cool',
+            password: Meteor.settings.baseUserPassword
+        }
+
+        Accounts.createUser(firstUser);
+
+        let firstUserId = Meteor.users.findOne({});
+
+        Meteor.users.update(firstUserId._id, {
+            $set: {
+                isAdmin: true
+            }
+        });
+    }
+});
